abd67e203b00db8fb648adfd7f896a82498d3dc2
semac <semac@SEMA>
2024-11-02 10:03:53 +0300

    Add project files.

---
+++ Project7/neww.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	char letter;
+	cout << "enter a letter:";
+	cin >> letter;
+	switch (letter) {
+	case 'a':cout << letter << " is a vowel\n"; break;
+	case 'e':cout << letter << " is a vowel\n"; break;
+	case 'u':cout << letter << " is a vowel\n"; break;
+	case 'o':cout << letter << " is a vowel\n"; break;
+	case 'i':cout << letter << " is a vowel\n"; break;
+	default:cout << letter << " is a consonant";
+		cout << " or not a letter\n";
+	}
+	return 0;
+
+
+}
